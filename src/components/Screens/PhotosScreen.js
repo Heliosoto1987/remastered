8086b53620791b3bbe11401757236cd0fc1833f2
Helio <heliosoto17@gmail.com>
2021-03-08 05:18:16 +0100

    remove build

--- src/components/Screens/PhotosScreen.js
+++ src/components/Screens/PhotosScreen.js
@@ -6,7 +6,7 @@ export const PhotosScreen = () => {
     <>
 
     <div className="photoScreen">
-    <a className="btn" href="./assets/HelioSoto.jpg" download>Descargar</a>
+    <a className="btn" href="/assets/HelioSoto.jpg" download>Descargar</a>
         <img src="https://github.com/Heliosoto1987/images/blob/master/Helio%20Soto.jpg?raw=true"></img>
     </div>
 
